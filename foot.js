fe9ea501f384c284761643eddc1267b0ed555974
snowball000550 <snowbj1994@163.com>
2018-07-27 19:43:57 +0800

    社区页面♪(^∇^*)

---
+++ foot.js
@@ -0,0 +1,18 @@
+document.writeln("<div class='footbox'>");
+document.writeln("<div class='foot-logo'>");
+document.writeln("<img src='images/LOGO-big.png' alt=''>");
+document.writeln("</div>");
+document.writeln("<div class='line2'></div>");
+document.writeln("<div class='foot-contact'>");
+document.writeln("<span style='color: #fff;'><img src='images/girl.png' class='small-img' alt=''> CONTACT:snowball</span>");
+document.writeln("<span style='color: #fff;'><img src='images/icon-2.png' class='small-img' alt=''> E-mail:snowbj1994@163.com</span>");
+document.writeln("<span style='color: #fff;'><img src='images/icon-3.png' class='small-img' alt=''> tel-(+86)13681115889</span>");
+document.writeln("<span style='color: #fff;'><img src='images/leave.png' class='small-img' alt=''> 此处为练习项目，欢迎各位hr小姐姐小哥哥来撩~</span>");
+document.writeln("</div>");
+document.writeln("<div class='line2'></div>");
+document.writeln("<div class='wechat'>");
+document.writeln("<div class='codeimg'>");
+document.writeln("<img src='images/wechat.jpeg' alt=''>");
+document.writeln("</div>");
+document.writeln("</div>");
+document.writeln("</div>");
